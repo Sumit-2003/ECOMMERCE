def2a1e18865a84eebe132236df2e0b992c29418
Sumit-2003 <vu3t4s2021033@pvppcoe.ac.in>
2024-08-24 05:47:01 +0530

    Fetch Category data

--- NexsusEcommerce/NexsusEcommerce/Program.cs
+++ NexsusEcommerce/NexsusEcommerce/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using NexsusEcommerce.Models; // Make sure to include this using directive
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add DbContext to the service container
+builder.Services.AddDbContext<EcommerceContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
