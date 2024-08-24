a29602c437394df421637bc53a299bc476c70949
Sumit-2003 <vu3t4s2021033@pvppcoe.ac.in>
2024-08-24 20:16:00 +0530

    Category Updated

--- NexsusEcommerce/NexsusEcommerce/Controllers/CategoryController.cs
+++ NexsusEcommerce/NexsusEcommerce/Controllers/CategoryController.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NexsusEcommerce.Models;
+using System.Linq;
 
 namespace NexsusEcommerce.Controllers
 {
     public class CategoryController : Controller
     {
-       
         private readonly EcommerceContext _context;
 
         public CategoryController(EcommerceContext context)
@@ -13,10 +13,77 @@ namespace NexsusEcommerce.Controllers
             _context = context;
         }
 
+        // Display all categories
         public IActionResult Index()
         {
-            var categories = _context.Categories.ToList(); // Assuming _context is your database context
+            var categories = _context.Categories.ToList();
             return View(categories);
         }
+
+        // GET: Category/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Category/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: Category/Edit/5
+        public IActionResult Edit(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Category/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Category category)
+        {
+            if (id != category.CategoryId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: Category/Delete/5
+        public IActionResult Delete(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
     }
 }
